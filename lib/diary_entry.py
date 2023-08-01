232749ac44a870c11ee7f0cb859061cbdb2ccaf6
elliepriestley <ellie.priestley@gmail.com>
2023-08-01 14:47:18 +0100

    Finished Unit Testing for Diary_Entry and TasktoDo Classes

--- lib/diary_entry.py
+++ lib/diary_entry.py
@@ -1,18 +1,9 @@
 class DiaryEntry:
 
-    def __init__(self, title, content, *contact):
-        # Parameters:
-        #   title: string representing the title of the diary entry
-        #   content: string representing the contents of the diary entry
-        #   *contact: an optional argument that is a dictionary. Key represents the name of contact, value represents the phone number
-        # Side-effects:
-        #   Sets the title, content and contact properties
+    def __init__(self, title, content, contact={}):
         self.title = title
         self.content = content
         self.contact = contact
-        if contact == True:
-            self.contact[contact[0]] = contact[1]
-            
 
 
     def count_words(self):
@@ -26,7 +17,7 @@ class DiaryEntry:
             return self.count_words() / wpm
 
     def contact_included(self):
-        # Parameters: None
-        # Returns:
-        #   boolean based on whether the instance of Diary Entry used the optional contact argument. 
-        pass
+        if self.contact != {}:
+            return True
+        else:
+            return False
--- tests/test_diary_entry.py
+++ tests/test_diary_entry.py
@@ -39,5 +39,39 @@ Test that it is possible to use optional argument contact to pass through a dict
 """
 
 def test_diary_entry_can_take_dictionary_as_optional_argument():
-        entry3 = DiaryEntry("Title", "One two three four five six seven eight nine ten", {"Marie":"0798625116"})
-        assert entry3.contact == {"Marie":"0798625116"}
+    entry3 = DiaryEntry("Title", "One two three four five six seven eight nine ten", {'Marie':'0798625116'})
+    assert entry3.contact == {"Marie":"0798625116"}
+
+
+"""
+Test if a diary entry does not have a contact number contained within it and the user tries to access it, returns empty dictionary
+"""
+
+def test_for_empty_dict_if_user_accesses_contact_where_none_exists():
+    entry4 = DiaryEntry("Title", "Contents are here")
+    assert entry4.contact == {}
+
+"""
+Test that contact_included returns True when there has been a contact provided
+"""
+
+def test_contact_included_returns_true_if_contact_argument_passed_through():
+    entry5 = DiaryEntry("Title", "Here are some contents", {'Emily': '0798557654'})
+    entry6 = DiaryEntry("Title2", "Here are some more contents", {'Sarah': '089274754'})
+    entry7 = DiaryEntry("Title3", "Here are some more more contents", {'Mohsina': '9083432648'})
+    assert entry5.contact_included() == True 
+    assert entry6.contact_included() == True 
+    assert entry7.contact_included() == True 
+
+
+"""
+Test contact_included returns False where there has not been a contact provided
+"""
+
+def test_contact_included_returns_false_if_contact_argument_not_passed_through():
+    entry1 = DiaryEntry("Title", "Here are some contents")
+    entry2 = DiaryEntry("Title", "Contents 2")
+    entry3 = DiaryEntry("Title", "One two three")
+    assert entry1.contact_included() == False
+    assert entry2.contact_included() == False
+    assert entry3.contact_included() == False
